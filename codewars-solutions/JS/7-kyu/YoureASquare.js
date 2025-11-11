c144f16ab7bb14dcd6a99dda36f0c8c8d0af77db
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2025-11-11 23:56:18 +0300

    feat: add You're a square solution

--- codewars-solutions/JS/7-kyu/YoureASquare.js
+++ codewars-solutions/JS/7-kyu/YoureASquare.js
@@ -7,13 +7,13 @@ of an integer; in other words, it is the product of some integer with itself.
 The tests will always use some integral number, so don't worry about that in dynamic
 typed languages.
 
-Examples
--1  =>  false
- 0  =>  true
- 3  =>  false
- 4  =>  true
-25  =>  true
-26  =>  false */
+  Examples:
+  -1  =>  false
+  0  =>  true
+  3  =>  false
+  4  =>  true
+  25  =>  true
+  26  =>  false */
 var isSquare = function (n) {
   return n >= 0 && Math.sqrt(n) % 1 === 0;
 };
