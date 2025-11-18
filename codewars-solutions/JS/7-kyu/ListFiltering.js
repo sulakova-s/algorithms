4ecdf981ec95a9f6a20b9a436ab6f45ac76afd8b
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2025-11-19 01:23:14 +0300

    feat: add List Filtering solution

--- codewars-solutions/JS/7-kyu/ListFiltering.js
+++ codewars-solutions/JS/7-kyu/ListFiltering.js
@@ -1,40 +1,17 @@
 /* 
-  An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+  In this kata you will create a function that takes a list of non-negative integers
+  and strings and returns a new list with the strings filtered out.
 
-  Example:
-    "Dermatoglyphics" --> true
-    "aba" --> false
-    "moOse" --> false (ignore letter case)
+  Example
+    filter_list([1,2,'a','b']) == [1,2]
+    filter_list([1,'a','b',0,15]) == [1,0,15]
+    filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
-// Reduce solution
-function isIsogramReduce(str) {
-  return str
-    .toUpperCase()
-    .split("")
-    .sort()
-    .reduce((acc, current, index, array) => {
-      return acc && (index === 0 || current !== array[index - 1]);
-    }, true);
-}
-
-// Set solution
-function isIsogramSet(str) {
-  return new Set(str.toUpperCase()).size === str.length;
+function filter_list(list) {
+  return list.filter(item => typeof item === 'number');
 }
 
 // Test cases
-console.log(isIsogramReduce("Dermatoglyphics")); // true
-console.log(isIsogramReduce("isogram")); // true
-console.log(isIsogramReduce("aba")); // false
-console.log(isIsogramReduce("moOse")); // false
-console.log(isIsogramReduce("isIsogram")); // false
-console.log(isIsogramReduce("")); // true
-
-console.log();
-
-console.log(isIsogramSet("Dermatoglyphics")); // true
-console.log(isIsogramSet("isogram")); // true
-console.log(isIsogramSet("aba")); // false
-console.log(isIsogramSet("moOse")); // false
-console.log(isIsogramSet("isIsogram")); // false
-console.log(isIsogramSet("")); // true
+console.log(filter_list([1,2,'a','b'])); // [1,2]
+console.log(filter_list([1,'a','b',0,15])); // [1,0,15]
+console.log(filter_list([1,2,'aasf','1','123',123])); // [1,2,123]
