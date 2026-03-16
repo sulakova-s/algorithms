d7152e6bf3194468233a467cea7f9e6065a4d266
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2026-03-16 22:52:31 +0300

    docs: add function description comment to You Only Need One solution

--- codewars-solutions/TypeScript/8-kyu/YouOnlyNeedOne.ts
+++ codewars-solutions/TypeScript/8-kyu/YouOnlyNeedOne.ts
@@ -8,6 +8,8 @@
 */
 export const check = (a: (number | string)[], x: number | string): boolean => {
   return a.includes(x);
+
+  // Alternative solutions:
   // return a.find((element) => element === x) !== undefined;
   // return a.indexOf(x) !== -1;
 };
@@ -17,4 +19,3 @@ console.log(check([66, 101], 66)); // true
 console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45)); // true
 console.log(check(["t", "e", "s", "t"], "e")); // true
 console.log(check(["what", "a", "great", "kata"], "kat")); // false
-
